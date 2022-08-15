2d341bd6c4cb8d63461ba308ff1c544e6f86518f
smorales <86622799+smorales-playtech@users.noreply.github.com>
2022-08-15 14:24:33 -0500

    backend en nest

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,17 @@ const usuario = require('./routes/usuario');
 const deportista = require('./routes/deportista');
 
 var app = express();
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 app.use(bodyParser.json());
 app.use("/usuario", usuario)
 app.use("/deportista", deportista)
 
 app.listen(3000);
+
