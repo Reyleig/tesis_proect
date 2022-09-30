0073ecbb79a44052fa26fce7a0c18f55177c80ad
smorales <50756544+Reyleig@users.noreply.github.com>
2022-09-30 08:11:12 -0500

    Update login.page.ts

--- frontend/src/app/login/login.page.ts
+++ frontend/src/app/login/login.page.ts
@@ -27,11 +27,6 @@ export class LoginPage implements OnInit, OnDestroy {
   token!: string;
   private valueSuscription: Subscription;
 
-  // const phone = new FormControl('', Validators.compose([
-  //   Validators.required,
-  //   PhoneValidator.invalidCountryPhone(country)
-  // ]));
-
   public userForm: UntypedFormGroup = new UntypedFormGroup({
     email: new UntypedFormControl('', { nonNullable: true }),
     password: new UntypedFormControl('', { nonNullable: true }),
@@ -39,9 +34,8 @@ export class LoginPage implements OnInit, OnDestroy {
 
   ishidden = false;
   user: any;
-  // SHA256 = require("crypto-js/sha256");
- hashDigest = sha256('Message');
- hmacDigest = Base64.stringify(hmacSHA512('Message', 'Secret Passphrase'));
+  hashDigest = sha256('Message');
+  hmacDigest = Base64.stringify(hmacSHA512('Message', 'Secret Passphrase'));
 
   constructor(
     private loginService: LoginService,
@@ -58,29 +52,21 @@ export class LoginPage implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.validarToken();
-    console.log( sha256('Message'));
-
-    console.log(this.hmacDigest);
-    console.log('asdfasdf');
   }
   ngOnDestroy() {
     this.valueSuscription.unsubscribe();
-
-
   }
 
   validarToken() {
     setTimeout(() => {
-      this.token$.subscribe((data: any) => {
-        if (data.token) {
-          console.log(data.token);
-
-          this.router.navigateByUrl('/inicio');
-        } else {
+      this.token$
+        .subscribe((data: any) => {
+          if (data.token) {
+            this.router.navigateByUrl('/inicio');
+          }
           this.ishidden = true;
-        }
-      }).unsubscribe;
-      console.log(this.ishidden);
+        })
+        .unsubscribe();
     }, 2000);
   }
 
@@ -90,26 +76,24 @@ export class LoginPage implements OnInit, OnDestroy {
     idrol: number | null
   ): void {
     if (token) {
-      console.log(token, username, idrol);
-
       this.store.dispatch(new AddUser(token, username, idrol));
     }
   }
 
   login() {
-    //encriptar password con sha256
     const password = this.userForm.get('password').value;
     const hashPassword = sha256(password);
     const hashPasswordBase64 = Base64.stringify(hashPassword);
-// agregar al formulario la contraceña encriptada
     this.userForm.get('password').setValue(hashPasswordBase64);
-    console.log(this.userForm.value);
 
     this.loginService.login(this.userForm.value).subscribe((response) => {
       if (response) {
         this.addUser(response.access_token, response.username, response.idrol);
-        // this.router.navigateByUrl('/inicio');
+        this.userForm.get('password').setValue('');
+        this.userForm.get('email').setValue('');
+        this.router.navigateByUrl('/inicio');
       } else {
+        this.userForm.get('password').setValue(password);
         this.utilitiesService.errorAlert(
           'Credenciales invalidas',
           'Intente de nuevo'
