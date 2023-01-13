e75961ce602af973c53f6d93d5be5a630b118a69
DavidArguello1321 <est.david.arguello@unimilitar.edu.co>
2023-01-13 18:23:08 -0500

    creacion de la entidad categoria

--- backend/src/category/entities/category.entity.ts
+++ backend/src/category/entities/category.entity.ts
@@ -1,1 +1,26 @@
-export class Category {}
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    Table,
+    BeforeInsert,
+  } from 'typeorm';
+  import * as bcrypt from 'bcrypt';
+  @Entity('categorias')
+export class Category {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    descripcion_categoria: string;
+
+    constructor(
+        id: number, descripcion_categoria: string,
+    ){
+        this.id = id;
+        this.descripcion_categoria = descripcion_categoria;
+    }
+
+
+}
